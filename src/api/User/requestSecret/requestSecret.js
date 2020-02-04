7e9683ec8537ab4fb1fec6962640819d9e76f9d9
Jetty2020 <kmclo@naver.com>
2020-02-04 20:44:05 +0900

    #3.7 toggleLike Resolver part One

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,11 +3,10 @@ import { generateSecret, sendSecretMail } from "../../../utils";
 
  export default {
    Mutation: {
-    requestSecret: async (_, args, { request }) => {
+    requestSecret: async (_, args) => {
        const { email } = args;
        const loginSecret = generateSecret();
        try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
          await prisma.updateUser({ data: { loginSecret }, where: { email } });
          return true;
