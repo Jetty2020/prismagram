cb5a0b53d0ffc2d4ba7a657eace02dee9139f757
Jetty2020 <kmclo@naver.com>
2020-02-05 05:20:20 +0900

    #3.13 See Full Posts

--- src/fragments.js
+++ src/fragments.js
@@ -1,14 +1,10 @@
-export const USER_FRAGMENT = `
-     fragment UserParts on User{
-         id 
-         username
-         email
-         firstName
-         lastName
-         following { 
-             
-             caption
-         }
-     }
+export const COMMENT_FRAGMENT = `
+     fragment CommentParts on Comment{
+         id
+         text
+         user {
+             username
+        }
+    }
      
-   `;
+`;
