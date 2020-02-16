f2ff39a547e97c5fa5aa6180029f44e9f1d868cb
Jetty2020 <kmclo@naver.com>
2020-02-16 18:37:06 +0900

    Preparing for Upload

--- src/server.js
+++ src/server.js
@@ -5,15 +5,18 @@ import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
+import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
     schema,
-    context: ({ request }) => ({ request, isAuthenticated })  });
+    context: ({ request }) => ({ request, isAuthenticated })  
+});
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
 console.log(`✅ Server running on http://localhost:${PORT}`)
